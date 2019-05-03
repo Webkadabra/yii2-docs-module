dcdc1cbe9b37b37bc7317f9cb04dbfd9b8fb7a94
Sergii Gama <balls@sergiigama.com>
2019-05-03 15:16:56 +0300

    Fix generated links when `docs` is inside of another module

--- controllers/DocsController.php
+++ controllers/DocsController.php
@@ -85,7 +85,7 @@ class DocsController  extends Controller
                     ],
                     ]);
                     if ($fileLists) {
-                        $content[] = '## [' . mb_ucfirst($view_id).']('.urldecode(Yii::$app->urlManager->createAbsoluteUrl(['/docs/docs/index', 'page' => $view_id], 1)).')';
+                        $content[] = '## [' . mb_ucfirst($view_id).']('.urldecode(\yii\helpers\Url::toRoute(['index', 'page' => $view_id], 1)).')';
                         $links = [];
                         foreach ($fileLists as $fileName) {
                             $view_id2 = str_ireplace($viewPath, '', $fileName);
@@ -96,7 +96,7 @@ class DocsController  extends Controller
                             $resolveTitle = str_ireplace($view_id, '', $view_id2);
                             $resolveTitle = ltrim($resolveTitle, '/');
 
-                            $link = '['.$resolveTitle.']('.urldecode(Yii::$app->urlManager->createAbsoluteUrl(['/docs/docs/index', 'page' => $view_id2], 1)).')';
+                            $link = '['.$resolveTitle.']('.urldecode(\yii\helpers\Url::toRoute(['index', 'page' => $view_id2], 1)).')';
                             $links[$resolveTitle] = $link;
                         }
                         if ($links) {
@@ -141,7 +141,7 @@ class DocsController  extends Controller
                         $view_id2 = ltrim($view_id2, '/');
                         $resolveTitle = str_ireplace($view_id, '', $view_id2);
                         $resolveTitle = ltrim($resolveTitle, '/');
-                        $links[] = '['.$resolveTitle.']('.urldecode(Yii::$app->urlManager->createAbsoluteUrl(['/docs/docs/index', 'page' => $view_id2], 1)).')';
+                        $links[] = '['.$resolveTitle.']('.urldecode(\yii\helpers\Url::toRoute(['index', 'page' => $view_id2], 1)).')';
                     }
                     $tpl = Yii::t('app', 'Index') . "\n\n* " . implode("\n* ", $links);
                     $content = Markdown::process($tpl, 'extra');
@@ -233,7 +233,7 @@ class DocsController  extends Controller
                     $view_id2 = ltrim($view_id2, '/');
                     $resolveTitle = str_ireplace($view_id, '', $view_id2);
                     $resolveTitle = ltrim($resolveTitle, '/');
-                    $links[] = '['.$resolveTitle.']('.urldecode(Yii::$app->urlManager->createAbsoluteUrl(['/docs/docs/index', 'page' => $view_id2], 1)).')';
+                    $links[] = '['.$resolveTitle.']('.urldecode(\yii\helpers\Url::toRoute(['index', 'page' => $view_id2], 1)).')';
                 }
                 $tpl = Yii::t('app', 'Index') . "\n\n* " . implode("\n* ", $links);
                 $content = Markdown::process($tpl, 'extra');
